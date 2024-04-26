1d52beb93add4d5a689d423d5c2de00c499be404
xiaoxlh <luoruikun8633@qq.com>
2024-04-27 01:30:05 +0800

    v1.0.1

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+ 
+from pkg_resources import parse_requirements
+
+install_requires = ['requests==2.31.0']
+ 
+setup(
+    name="Epate",
+    version="1.0.1",
+    author="xiaoxlh",
+    author_email="xiaoxiaogzs@outlook.com",
+    description="A python library for many API of the codemao's website.",
+    long_description="eds sdk for python",
+    license="Apache License, Version 2.0",
+    url="https://xiao.asia",
+ 
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    
+    packages=find_packages(),
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'test = test.help:main'
+        ]
+    }
+)
